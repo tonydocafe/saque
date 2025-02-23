84fb705a7a9a8facb9dce5c5d3000e483d42a042
Tony Júnior <155020337+tonydocafe@users.noreply.github.com>
2025-02-22 21:53:05 -0300

    visão

---
+++ view.rs
@@ -0,0 +1,20 @@
+pub fn display_prompt() {
+    println!("Digite o valor a ser sacado:");
+    println!("( apenas notas! )");
+}
+
+pub fn display_bills(bills: &[(i32, i32)]) {
+    for (cedula, quantidade) in bills {
+        println!("Cédulas de R$ {}: {}", cedula, quantidade);
+    }
+}
+
+pub fn display_message(message: &Option<String>) {
+    if let Some(msg) = message {
+        println!("{}", msg);
+    }
+}
+
+pub fn display_error(error: &str) {
+    eprintln!("{}", error);
+}
